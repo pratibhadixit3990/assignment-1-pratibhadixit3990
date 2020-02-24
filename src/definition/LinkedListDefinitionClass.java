bc5ee2d13b6b322814d82da4ca4d6d86e8920427
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-02-24 18:32:38 +0530

    commit: LinkedListDefinitionClass implements LinkedListADTInterface and overrides it's methods

--- src/definition/LinkedListDefinitionClass.java
+++ src/definition/LinkedListDefinitionClass.java
@@ -1,4 +1,31 @@
 package definition;
 
-public class LinkedListDefinitionClass {
+import adt.LinkedListADTInterface;
+
+public class LinkedListDefinitionClass implements LinkedListADTInterface {
+
+    @Override
+    public void add(String fName, String lName, String[] cNumberArray, String eMAddress) {
+
+    }
+
+    @Override
+    public void remove() {
+
+    }
+
+    @Override
+    public void search(String searchingContact) {
+
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
