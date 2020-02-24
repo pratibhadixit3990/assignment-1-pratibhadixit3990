7d4d73369745efefe36f5f10ced71d5399eef695
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-02-24 18:51:02 +0530

    commit: a new object of LinkedListDefinitionClass of name list declared

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,7 +1,12 @@
 package person;
 
+import definition.LinkedListDefinitionClass;
+
 import java.util.Scanner;
 
 public class Person {
     final static Scanner scanner = new Scanner(System.in);
+
+    LinkedListDefinitionClass list = new LinkedListDefinitionClass();
+
 }
