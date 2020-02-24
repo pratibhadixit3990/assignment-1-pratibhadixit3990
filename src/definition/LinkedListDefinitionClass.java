5ffd1551f5821e9379a800be8530f3b11a70620e
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-02-24 18:33:43 +0530

    commit: field first of type link declared

--- src/definition/LinkedListDefinitionClass.java
+++ src/definition/LinkedListDefinitionClass.java
@@ -1,12 +1,14 @@
 package definition;
 
 import adt.LinkedListADTInterface;
+import link.Link;
 
 public class LinkedListDefinitionClass implements LinkedListADTInterface {
 
+    private Link first;
+
     @Override
     public void add(String fName, String lName, String[] cNumberArray, String eMAddress) {
-
     }
 
     @Override
