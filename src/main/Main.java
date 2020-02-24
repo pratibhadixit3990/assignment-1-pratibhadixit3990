8a79dd2188e87fd56a455d285c00d6cc6aebdbd5
pratibhadixit3990 <pratibha.dixit_cs18@gla.ac.in>
2020-02-24 19:14:22 +0530

    commit: contactList calls to the method contactListApp of the person class

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,5 +5,6 @@ import person.Person;
 public class Main extends Person {
     public static void main(String[] args) {
         Person contactList = new Person();
+        contactList.contactListApp();
     }
 }
